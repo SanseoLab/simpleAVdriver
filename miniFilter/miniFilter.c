cea296052ab6bd3decb056a3a57a91cc212b207c
ej <101196@live.jnu.ac.kr>
2017-09-28 15:50:39 +0900

    Update miniFilter.c

--- miniFilter/miniFilter.c
+++ miniFilter/miniFilter.c
@@ -1,13 +1,10 @@
 #include "miniFilter.h"
 
 NTSTATUS DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING RegistryPath) {
-
-NTSTATUS status;
+	NTSTATUS status;
 	UNREFERENCED_PARAMETER(RegistryPath);
 
-status = FltRegisterFilter(DriverObject,
-		&filter_registration,
-		&driver_data.filter);
+	status = FltRegisterFilter(DriverObject, &filter_registration, &driver_data.filter);
 
 	if (NT_SUCCESS(status)) {
 		status = FltStartFiltering(driver_data.filter);
@@ -15,7 +12,7 @@ status = FltRegisterFilter(DriverObject,
 		if (!NT_SUCCESS(status)) {
 			FltUnregisterFilter(driver_data.filter);
 		}
-		DbgPrint("##newDriver [ miniFilter ] Start Filtering \n");
+		DbgPrint("[ miniFilter ] Start Filtering \n");
 	}
 
 	return status;
